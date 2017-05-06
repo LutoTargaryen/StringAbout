14303e88b8db01771517ae9b536d13eb99a10c45
16014 <16014@192.168.137.238>
2017-05-06 19:19:08 +0800

    String,StringBuilder,StringBuffer效率比较

---
+++ src/com/luto/string/SP.java
@@ -0,0 +1,13 @@
+package com.luto.string;
+
+public class SP {
+	public String str;
+	public StringBuilder builder;
+	public StringBuffer buffer;
+	
+	public void StringP(){
+		
+		
+	}
+	
+}
---
+++ src/com/luto/string/Test.java
@@ -0,0 +1,34 @@
+package com.luto.string;
+
+import java.util.Date;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		test1();
+	}
+
+	/**
+	 * 测试String，StringBuilder，StringBuffer的效率
+	 */
+	private static void test1() {
+		String str = "1";
+		StringBuilder builder = new StringBuilder("1");
+		StringBuffer buffer = new StringBuffer("1");
+		Date strdate = new Date();
+		for (int i = 0; i < 10000;i++) {
+			str += i;
+		}
+		System.out.println("String : " + ((new Date()).getTime() - strdate.getTime()));
+		Date builderdate = new Date();
+		for(int i = 0;i<10000;i++){
+			builder.append(i);
+		}
+		System.out.println("StringBuilder : " + ((new Date()).getTime() - builderdate.getTime()));
+		Date bufferdate = new Date();
+		for(int i = 0;i<10000;i++){
+			buffer.append(i);
+		}
+		System.out.println("StringBuffer : " + ((new Date()).getTime() - bufferdate.getTime()));
+	}
+}
